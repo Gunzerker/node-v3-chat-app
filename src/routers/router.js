09bc157c2178e43fa5137d7cd98473638eb4b18d
zerai houssem <houssemzerai@gmail.com>
2019-10-25 22:08:51 +0100

    Fixed some bugs

--- src/routers/router.js
+++ src/routers/router.js
@@ -13,14 +13,58 @@ var rooms=[]
 
 
 router.post('/register',async(req,res)=>{
+    var already=0
+    await firebase.auth().createUserWithEmailAndPassword(req.body.mail, req.body.psw).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(error.code,+" ",+errorMessage)
 
-    firebase.auth().createUserWithEmailAndPassword(req.body.mail, req.body.psw).catch(function(error) {
+  if (!errorCode){
+    already=0
+    
+  }
+  else{
+    already=1
+    
+  }
+  // ...
+})
+
+if(already==0){
+await firebase.auth().signInWithEmailAndPassword(req.body.mail, req.body.psw).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
   // ...
-})
-    
+ 
+});
+
+await firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    logedin.push(user)
+    current[0]=user
+    console.log('yes')
+    valid=1
+    // ...
+  } else {
+    valid=0
+    // User is signed out.
+    // ...
+  }
+});
+  res.redirect('/?usermail='+req.body.mail)
+}
+  else
+  res.redirect('/register.html?exist=1')
 })
 router.post('/login',async(req,res)=>{
 
@@ -29,6 +73,7 @@ router.post('/login',async(req,res)=>{
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
+       
       });
       
     await firebase.auth().onAuthStateChanged(function(user) {
